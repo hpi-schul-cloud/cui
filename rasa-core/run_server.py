e0dffd68c057aa601587b020040bc836220b3fbb
Felix Musmann <felix@musmann.io>
2018-06-18 12:42:49 +0200

    Update rasa_core and route for intent_ranking from latest message

--- rasa-core/run_server.py
+++ rasa-core/run_server.py
@@ -1,6 +1,11 @@
 from os import environ as env
-from rasa_core.server import RasaCoreServer
+from gevent.pywsgi import WSGIServer
+
+from server import create_app
+from rasa_core import utils
 from rasa_core.interpreter import RasaNLUHttpInterpreter
+
+utils.configure_colored_logging("DEBUG")
 
 nlu_interpreter = RasaNLUHttpInterpreter(
     model_name = env["RASA_NLU_MODEL_NAME"],
@@ -8,9 +13,12 @@ nlu_interpreter = RasaNLUHttpInterpreter(
     server = env["RASA_NLU_SERVER_ADDRESS"],
     project_name = env["RASA_NLU_PROJECT_NAME"])
 
-rasa = RasaCoreServer(
+app = create_app(
     model_directory = env["RASA_CORE_MODEL_PATH"],
+    cors_origins="*",
     loglevel = "DEBUG",
     logfile = "./logs/rasa_core.log",
     interpreter = nlu_interpreter)
-rasa.app.run("0.0.0.0", 5005)
+
+http_server = WSGIServer(('0.0.0.0', 5005), app)
+http_server.serve_forever()
